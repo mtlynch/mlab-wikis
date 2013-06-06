a4322d9bdecc71ba19931e475e1117ff9433d306
Dominic Hamon <dma+github@stripysock.com>
2013-06-06 13:00:55 -0700

    mbm is in its own repo

--- mbm/src/common/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_CONSTANTS_H_
-#define COMMON_CONSTANTS_H_
-
-#define MBM_VERSION "0.2"
-#define END_OF_LINE "*** END OF LINE ***"
-#define READY       "READY"
-
-#endif  // COMMON_CONSTANTS_H_
